b49bb3d37296ad0e57e1d9ff62f3cdd7ee97819a
PajdicJelena1947 <pajdicjelena@gmail.com>
2015-06-05 17:34:04 +0200

    Update GenericDaoImpl.java
    IT250-DZ10

--- src/main/java/com/mycompany/it250/dz/dao/GenericDaoImpl.java
+++ src/main/java/com/mycompany/it250/dz/dao/GenericDaoImpl.java
@@ -25,6 +25,12 @@ public class GenericDaoImpl <T extends AbstractEntity> implements GenericDao<T> 
     @Inject
     private Session hibernate;
     
+     @Override
+    public T merge(T obj) {
+        return (T) hibernate.merge(obj);
+    }
+
+    
    
     @Override
     public T delete(Integer idOfObj, Class klasa) {
